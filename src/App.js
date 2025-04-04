d39dc293e55cb58d5996ed78a6eb71a0a9953406
Abin Johnson <dynamo.exe@proton.me>
2025-04-04 19:49:24 +0530

    Most of the functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
 import Header from './components/Header';
 import Basket from './components/Basket';
 import Main from './components/Main';
+import data from './data';
+import { useState } from 'react';
 
 function App() {
+  const { products } = data;
+  const [cartItems, setCartItems] = useState([]);
+  const onAdd = (product) => {
+    const exist = cartItems.find(x => x.id === product.id);
+    if (exist) {
+      setCartItems(cartItems.map(x => x.id === product.id ? {...exist, qty: exist.qty + 1} : x))
+    } else {
+      setCartItems([...cartItems, {...product, qty: 1}])
+    }
+  }
+  const onRemove = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== product.id))
+    } else {
+      setCartItems(cartItems.map(x => x.id === product.id ? {...exist, qty: exist.qty - 1} : x))
+    }
+  }
+
   return (
     <div className="App">
       <Header></Header>
-      <div>
-      <Basket></Basket>
-      <Main></Main>
+      <div className='row'>
+      <Main onAdd={onAdd} products={products}></Main>
+      <Basket onAdd={onAdd} cartItems={cartItems} onRemove={onRemove}></Basket>
       </div>
     </div>
   );
